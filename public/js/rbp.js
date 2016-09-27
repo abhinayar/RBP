dac6ae7e06815f4fdc8ab851c88756b855857a0e
Abhi Nayar <abhi_nayar@Abhis-MacBook-Air.local>
2016-09-27 10:34:11 -0400

    09/27 Updates

--- public/js/rbp.js
+++ public/js/rbp.js
@@ -1,1 +1,29 @@
 //RBP js by Abhi Nayar
+
+
+$(document).ready(function(){
+    /* Map JS */
+   function init_map(){
+        var myOptions = {
+           zoom:15,
+           center:new google.maps.LatLng(35.122999,-89.78990499999998),
+           mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
+
+        marker = new google.maps.Marker({
+            map: map,position: new google.maps.LatLng(35.1217, -89.79),
+            icon: './public/img/common/rbpMapMarker.png'
+        });
+
+        infowindow = new google.maps.InfoWindow({content:"<b>The Right Brain People</b><br/>279 Norseman Drive<br/>Cordova, TN, 38018" });
+
+        google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});
+        infowindow.open(map,marker);
+   
+   }
+    
+    google.maps.event.addDomListener(window, 'load', init_map); 
+    
+});
