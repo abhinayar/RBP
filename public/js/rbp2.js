07c221f7c710ca2e320a49f316a4dd2a8195b3fa
Abhi Nayar <abhinayar@abhis-air.wireless.yale.internal>
2016-11-01 02:38:10 -0400

    Fixed nav issues and added modal

--- public/js/rbp2.js
+++ public/js/rbp2.js
@@ -28,3 +28,45 @@ $(document).ready(function(){
         $("ul.desktop-nav-ul").toggleClass("closed");
     })
 });
+
+$(window).load(function(){
+    $("body").addClass("loading");
+    $(".icon").removeClass("hidden");
+    
+    var snap = Snap.select('.loader');
+    var arc1 = snap.select('#arc1');
+    var arc2 = snap.select('#arc2');
+    var circle = snap.select('#circle');
+
+    var len1 = arc1.getTotalLength();
+    var len2 = arc2.getTotalLength();
+
+    arc1.attr({
+        'stroke-dasharray': len1,
+        'stroke-dashoffset': len1
+      }).animate({
+        'stroke-dashoffset': 0
+      }, 2500, mina.easeinout);
+
+    arc2.attr({
+        'stroke-dasharray': len2,
+        'stroke-dashoffset': len2
+      }).animate({
+        'stroke-dashoffset': 0
+      }, 2500, mina.easeinout);
+
+
+
+    arc1.animate({ d: "M39.198,64c0-13.709,11.093-24.802,24.802-24.802 M88.803,64c0-13.709-11.094-24.802-24.803-24.802" }, 2500, mina.easeinout);
+    arc2.animate({ d: "M88.803,64c0,13.708-11.094,24.802-24.803,24.802 M39.198,64c0,13.709,11.093,24.803,24.802,24.803" }, 2500, mina.easeinout);
+
+    setTimeout(function () {
+       circle.animate({r: 44}, 500, mina.bounce)
+    }, 1250);
+    
+    setTimeout(function(){
+        $("body").removeClass("loading");
+        $(".icon").hide().addClass("hidden");
+        $(".non-loader-wrapper").hide().removeClass("hidden").fadeIn("500");
+    }, 1250);
+})
